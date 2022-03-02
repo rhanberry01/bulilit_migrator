11b6eff0bf1b1daa16974b8d72c3a306aecd69ab
rhanm <srsdevteam2021@gmail.com>
2022-03-02 15:23:05 +0800

    test3

--- application/controllers/Auto.php
+++ application/controllers/Auto.php
@@ -801,7 +801,7 @@ provided that both dates are after 1970. Also only works for dates up to the yea
 
     $now =   date('Y-m-d');
     $past_date = date('Y-m-d', strtotime('-30 days'));
-    $dates = $this->getDatesFromRange($now, $past_date);
+    $dates = $this->getDatesFromRange($past_date, $now);
     echo var_dump($dates);
 
    /* if($date==null) $date = date("Y-m-d", strtotime("-1 day"));
